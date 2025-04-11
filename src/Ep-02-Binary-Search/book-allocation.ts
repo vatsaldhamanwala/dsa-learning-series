59de83e5b0d3836687d862b4e3288e9bb4117fc5
vatsaltyrannix <vatsal@tyrannix.com>
2025-04-11 19:03:36 +0530

    added another soloution related to book allocation problem.

--- src/Ep-02-Binary-Search/book-allocation.ts
+++ src/Ep-02-Binary-Search/book-allocation.ts
@@ -1,3 +1,4 @@
+//book-allocation
 function isPossibleSolution(arr: number[], mid: number, m: number): boolean {
   let studentCount = 1;
   let pageSum = 0;
@@ -46,3 +47,53 @@ function bookAllocation(arr: number[], m: number): number {
 const bookArray = [10, 20, 30, 40];
 const bookAnswer = bookAllocation(bookArray, 2);
 console.log('🚀 ~ bookAnswer:', bookAnswer);
+
+//Q.2-chapter allocation problem
+// function isPossibleSolution(arr: number[], mid: number, n: number): boolean {
+//   let daysCount = 1;
+//   let timeSum = 0;
+
+//   for (let i = 0; i < arr.length; i++) {
+//     if (timeSum + arr[i] <= mid) {
+//       timeSum += arr[i];
+//     } else {
+//       daysCount++;
+//       if (daysCount > n || arr[i] > mid) {
+//         return false;
+//       }
+//       timeSum = arr[i];
+//     }
+//   }
+
+//   return true;
+// }
+
+// function chapterAllocation(arr: number[], n: number): number {
+//   let start = 0;
+//   let sum = 0;
+
+//   for (let i = 0; i < arr.length; i++) {
+//     sum += arr[i];
+//   }
+
+//   let end = sum;
+
+//   let answer = -1;
+
+//   while (start <= end) {
+//     let mid = Math.floor((start + end) / 2);
+
+//     if (isPossibleSolution(arr, mid, n)) {
+//       answer = mid;
+//       end = mid - 1;
+//     } else {
+//       start = mid + 1;
+//     }
+//   }
+//   return answer;
+// }
+
+// // main
+// const timeArray = [1, 2, 2, 3, 1];
+// const chapterAnswer = chapterAllocation(timeArray, 3);
+// console.log('chapter answer is: ', chapterAnswer);
